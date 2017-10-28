976567ac71a6e80733b5b7696200472a8152ad73
ErikKakocz <erik.kakocz@gmail.com>
2017-10-28 07:51:43 +0200

    purged from devil

--- src/main/java/persistence/PersistenceManager.java
+++ src/main/java/persistence/PersistenceManager.java
@@ -5,34 +5,49 @@ import javax.persistence.EntityManagerFactory;
 
 import model.Szallas;
 import model.User;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.*;
 
 
 public class PersistenceManager {
 
-	static User getUserByName() {
-		User user=null;
+	private static EntityManager getEntityManager(){
 		EntityManagerFactory emf=Persistence.createEntityManagerFactory("TravelAgencyJpa");
 		EntityManager em=emf.createEntityManager();
+		return em;
+	}
+	
+	static User getUserByName() {
+		User user=null;
+		EntityManager em=getEntityManager();
 		
-		
-		
+		//getuser
 		
 		em.close();
 		return user;
 	}
 	
 	public void createUser(User user) {
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("TravelAgencyJpa");
-		EntityManager em=emf.createEntityManager();
+		EntityManager em=getEntityManager();
 		em.persist(user);
 		em.close();
 	}
 	
 	public void createSzallas(Szallas szallas) {
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("TravelAgencyJpa");
-		EntityManager em=emf.createEntityManager();
+		EntityManager em=getEntityManager();
 		em.persist(szallas);
 		em.close();
+	}
+	
+	public List<Szallas> getAllSzallas(){
+		EntityManager em=getEntityManager();
+		List<Szallas> szallasList=new LinkedList<Szallas>(); 
+		
+		
+		em.close();
+		return szallasList;
 	}
 }
