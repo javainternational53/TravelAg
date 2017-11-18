7e7609ea5debd0d0f66c454c3210ad9a03942178
ErikKakocz <erik.kakocz@gmail.com>
2017-11-18 15:09:38 +0100

    git pull elokeszitese

--- src/main/java/login/Main.java
+++ src/main/java/login/Main.java
@@ -1,9 +1,5 @@
 package login;
 
-
-// beágyazási str : srv - ver 1.2 
-
-
 import java.io.InputStream;
 
 import javafx.application.Application;
@@ -11,24 +7,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.User;
+import persistence.PersistenceManager;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+    	System.out.println("duplaasd");
     	//RS
     	//loader.setLocation(Main.class.getResourceAsStream("sample.fxml"));
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/sample.fxml"));
-
- 
 
         primaryStage.setScene(new Scene(root, 500, 775));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+    	User user=new User();
+        user.setUsername("asd");
+        user.setId(0);
+        
+        PersistenceManager.createUser(user);
+        user=null;
+        
+        user=PersistenceManager.getUserByName("asd");
+        
+        System.out.println(user);
+    	
+    	launch(args);
+         
     }
 }
--- src/main/java/persistence/PersistenceManager.java
+++ src/main/java/persistence/PersistenceManager.java
@@ -3,8 +3,9 @@ package persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import model.Szallas;
 import model.User;
+import model.Szallas;
+import model.Quality;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,34 +21,68 @@ public class PersistenceManager {
 		return em;
 	}
 	
-	static User getUserByName() {
+	public static User getUserByName(String name) {
 		User user=null;
 		EntityManager em=getEntityManager();
 		
-		//getuser
+		Query query = em.createQuery("SELECT u FROM User u WHERE name LIKE :uName");
+		query.setParameter("uName", name);
+		user=(User)query.getSingleResult();
 		
 		em.close();
 		return user;
 	}
 	
-	public void createUser(User user) {
+	public static void createUser(User user) {
 		EntityManager em=getEntityManager();
+		
 		em.persist(user);
+		
 		em.close();
 	}
 	
-	public void createSzallas(Szallas szallas) {
+	public static void createSzallas(Szallas szallas) {
 		EntityManager em=getEntityManager();
+		
 		em.persist(szallas);
+		
 		em.close();
 	}
 	
-	public List<Szallas> getAllSzallas(){
+	public static List<Szallas> getAllSzallas(){
 		EntityManager em=getEntityManager();
 		List<Szallas> szallasList=new LinkedList<Szallas>(); 
 		
+		Query query = em.createQuery("SELECT s from Szallas s");
+		szallasList = query.getResultList();
 		
 		em.close();
 		return szallasList;
 	}
+	
+	public static List<Szallas> getSzallasByLocation(String location){
+		EntityManager em=getEntityManager();
+		List<Szallas> szallasList=new LinkedList<Szallas>(); 
+		
+		Query query = em.createQuery("SELECT s from Szallas s WHERE location LIKE :loc");
+		query.setParameter("loc", location);
+		szallasList = query.getResultList();
+		
+		em.close();
+		return szallasList;
+	}
+	
+	public static List<Szallas> getSzallasByQuality(Quality quality){
+		EntityManager em=getEntityManager();
+		List<Szallas> szallasList=new LinkedList<Szallas>(); 
+		
+		Query query = em.createQuery("SELECT s from Szallas s WHERE location LIKE :qual");
+		query.setParameter("qual", quality);
+		szallasList = query.getResultList();
+		
+		em.close();
+		return szallasList;
+	}
+	
+	
 }
