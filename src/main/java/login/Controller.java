6b137038f0465541a5e33b31a93025e2da49c573
tamas9128 <32539573+tamas9128@users.noreply.github.com>
2017-11-23 10:00:14 +0100

    Update Controller.java

--- src/main/java/login/Controller.java
+++ src/main/java/login/Controller.java
@@ -10,7 +10,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -34,22 +37,25 @@ public class Controller implements Initializable {
 
 		String username = user.getText();
 		String pass = password.getText();
-
-		// Tesztelési fázis , mielőtt megkapja a dba
-
+		// Tesztelési fázis, mielőtt megkapja a dba
+		
 		if (username.equals("test") && pass.equals("test")) {
 
-			Parent root = FXMLLoader.load(this.getClass().getClassLoader().getResource("felulet.fxml"));
-			Stage main = new Stage();
-
-			main.setScene(new Scene(root, 500, 775));
-			main.show();
-
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/main.fxml"));
+			Scene scene = new Scene(root,1000,300);
+			primaryStage.setScene(scene);
+			primaryStage.show();
 			((Node) (event.getSource())).getScene().getWindow().hide();
 
 		} else
-			System.out.println("Wrong Password , Try it again ! ");
-
+		{
+			Alert alert = new Alert(AlertType.ERROR);
+	        alert.setTitle("Error");
+	        alert.setHeaderText("Adat megadási probléma");
+	        alert.setContentText("Nem tudtunk belépni.");
+	        alert.showAndWait();
+		}
 	}
 
 	public void initialize(URL url, ResourceBundle rb) {
