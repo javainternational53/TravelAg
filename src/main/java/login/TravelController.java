8ca480fb2cf6801583aec14b2b38c7296b57e2ec
javainternational53 <javainternational53@gmail.com>
2017-12-11 20:04:24 +0100

    Feature : search on the target

--- src/main/java/login/TravelController.java
+++ src/main/java/login/TravelController.java
@@ -31,5 +31,64 @@ public class TravelController {
 
 	private MainApp mainApp;
 
+
+	public TravelController() {
+	}
+
+	@FXML
+	private void initialize() {
+		// Initialize the person table with the two columns.
+		cityColumn.setCellValueFactory(cellData -> cellData.getValue().cityProperty());
+		minPriceColumn.setCellValueFactory(cellData -> cellData.getValue().minPriceProperty().asObject());
+		maxPriceColumn.setCellValueFactory(cellData -> cellData.getValue().maxPriceProperty().asObject());
+		hotelsNumberColumn.setCellValueFactory(cellData -> cellData.getValue().hotelsNumberProperty().asObject());
+
+		// Keres� vissza�ll�t�sa
+		this.searchButton.setOnAction((event) -> {
+			double minPrice = 1;
+			double maxPrice = 10000000;
+			String city = "";
+			if (minPriceInput.getText() != null)
+				try {
+					minPrice = Double.parseDouble(minPriceInput.getText());
+				} catch (NumberFormatException e) {
+					minPrice = 1.0;
+				}
+			if (maxPriceInput.getText() != null)
+				try {
+					maxPrice = Double.parseDouble(maxPriceInput.getText());
+				} catch (NumberFormatException e) {
+					maxPrice = 10000000;
+				}
+			if (cityInput.getText() != null)
+				try {
+					city = cityInput.getText();
+				} catch (Exception e) {
+					city = "";
+				}
+			for (Travel travel : mainApp.getTravelData()) {
+				if (minPrice <= travel.getMinPrice() && maxPrice >= travel.getMAxPrice()
+						&& (city.equals(travel.getCity()) || city.length() == 0)) {
+					System.out.println(travel.getCity());
+					
+					Stage primaryStage = new Stage();
+					Parent root = null;
+					try {
+						root = FXMLLoader.load(getClass().getClassLoader().getResource("travel/view/travelSearch.fxml"));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					Scene scene = new Scene(root,600,400);
+					primaryStage.setScene(scene);
+					primaryStage.setTitle("TravelSearch");
+					primaryStage.show();
+				
+				}
+			}
+			System.out.println();
+		});
+		
+	}
+
 	
 }
