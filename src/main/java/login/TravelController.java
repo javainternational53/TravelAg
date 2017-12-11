b8166bc653d0ea6c9a9854124f9832328882f947
javainternational53 <javainternational53@gmail.com>
2017-12-11 20:02:54 +0100

    Travelcontroller refact to new struct

--- src/main/java/login/TravelController.java
+++ src/main/java/login/TravelController.java
@@ -1,5 +1,35 @@
 package login;
 
 public class TravelController {
+
+	@FXML
+	List<Travel> travelList;
+	@FXML
+	private TableView<Travel> travelTable;
+	@FXML
+	private TableColumn<Travel, String> cityColumn;
+	@FXML
+	private TableColumn<Travel, Double> minPriceColumn;
+	@FXML
+	private TableColumn<Travel, Double> maxPriceColumn;
+	@FXML
+	private TableColumn<Travel, Integer> hotelsNumberColumn;
+
+	@FXML
+	private TextField minPriceInput = new TextField();
+	@FXML
+	private TextField maxPriceInput = new TextField();
+	@FXML
+	private TextField cityInput = new TextField();
+
+	@FXML
+	private Button searchButton = new Button();
+	@FXML
+	private Button deleteButton = new Button();
+
+	private ObservableList<Travel> travelsearchObservList = null;
+
+	private MainApp mainApp;
+
 	
 }
