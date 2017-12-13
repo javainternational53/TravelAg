5dbed7ea0c896465bd1d34008661fc486119753c
tamas9128 <32539573+tamas9128@users.noreply.github.com>
2017-12-13 17:31:23 +0100

    UserListController

---
+++ src/main/java/travelling/view/UserListController.java
@@ -0,0 +1,40 @@
+package travelling.view;
+
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import travelling.MainApp;
+import travelling.model.User;
+
+public class UserListController {
+	@FXML
+	List<User> travelList;
+	@FXML
+	private TableView<User> userTable;
+	@FXML
+	private TableColumn<User, String> userNameColumn;
+	@FXML
+	private TableColumn<User, String> passwordColumn;
+	
+	private MainApp mainApp;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private MenuController menuController;
+	
+	public UserListController() {
+	}
+	@FXML
+	private void initialize() {
+		userNameColumn.setCellValueFactory(cellData -> cellData.getValue().userNameProperty());
+		passwordColumn.setCellValueFactory(cellData -> cellData.getValue().passwordProperty());
+	}
+	public void setMenuController(MenuController menuController)
+	{
+		this.menuController = menuController;
+		userTable.setItems(menuController.getUserListData());
+	}
+}
