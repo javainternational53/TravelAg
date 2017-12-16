f364b80f879e29355dc91e3479d9b0289aad58ca
tamas9128 <32539573+tamas9128@users.noreply.github.com>
2017-12-16 18:46:16 +0100

    Add files via upload

--- src/main/java/travelling/view/RegisterController.java
+++ src/main/java/travelling/view/RegisterController.java
@@ -25,10 +25,21 @@ public class RegisterController {
 	private TextField fxEmail;
 	
 	@FXML
+	private TextField fxBankCardNumber;
+	
+	@FXML
 	private Button fxClear;
 	
 	@FXML
 	private Button fxSignUp;
+	
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String email;
+	private String bankCardNumber;
+	
 	
 	public void ReseAll(ActionEvent event) {
 		fxFirstName.setText("");
@@ -36,10 +47,126 @@ public class RegisterController {
 		fxUserName.setText("");
 		fxPassword.setText("");
 		fxEmail.setText("");
+		fxBankCardNumber.setText("");
 	}
+
 	private MainApp mainApp;
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBankCardNumber() {
+		return bankCardNumber;
+	}
+
+	public void setBankCardNumber(String bankCardNumber) {
+		this.bankCardNumber = bankCardNumber;
+	}
+
+	
+	
+	public TextField getFxFirstName() {
+		return fxFirstName;
+	}
+
+	public void setFxFirstName(TextField fxFirstName) {
+		this.fxFirstName = fxFirstName;
+	}
+
+	public TextField getFxLastName() {
+		return fxLastName;
+	}
+
+	public void setFxLastName(TextField fxLastName) {
+		this.fxLastName = fxLastName;
+	}
+
+	public TextField getFxUserName() {
+		return fxUserName;
+	}
+
+	public void setFxUserName(TextField fxUserName) {
+		this.fxUserName = fxUserName;
+	}
+
+	public TextField getFxPassword() {
+		return fxPassword;
+	}
+
+	public void setFxPassword(TextField fxPassword) {
+		this.fxPassword = fxPassword;
+	}
+
+	public TextField getFxEmail() {
+		return fxEmail;
+	}
+
+	public void setFxEmail(TextField fxEmail) {
+		this.fxEmail = fxEmail;
+	}
+
+
+	public TextField getFxBankCardNumber() {
+		return fxBankCardNumber;
+	}
+
+	public void setFxBankCardNumber(TextField fxBankCardNumber) {
+		this.fxBankCardNumber = fxBankCardNumber;
+	}
+
+	public void SignUp(ActionEvent event) {
+		setFirstName(fxFirstName.getText());
+		setLastName(fxLastName.getText());
+		setUserName(fxUserName.getText());
+		setPassword(fxPassword.getText());
+		setEmail(fxEmail.getText());
+		setBankCardNumber(fxBankCardNumber.getText());
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(email);
+		System.out.println(bankCardNumber);
+	}
 	
 }
